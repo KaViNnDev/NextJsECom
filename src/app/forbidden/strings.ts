c332156c490b80ddc84f2a6f07b74c2d8c44599e
Kavinkumar <kavin.n.dev@gmail.com>
2024-03-25 11:19:51 +0530

    feat: Added Forbidden page

---
+++ src/app/forbidden/strings.ts
@@ -0,0 +1,3 @@
+export const TITLE = "Forbidden";
+export const CONTENT = "Sorry, you're not allowed to view this page.";
+export const HOME_BUTTON = "Go back home";
---
+++ src/app/pageNotFound/strings.ts
@@ -0,0 +1,3 @@
+export const TITLE = "Page not found";
+export const CONTENT = "Sorry, we couldn't find the page you're looking for.";
+export const HOME_BUTTON = "Go back home";
