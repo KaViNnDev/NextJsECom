a355b4b74bbb19f937a0be3e53493293be300003
Kavinkumar <kavin.n.dev@gmail.com>
2024-03-25 13:17:52 +0530

    feat: configured typography and colors in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,77 @@ export default {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
+        inter: ["inter"],
+      },
+      fontSize: {
+        "app-title": [
+          "32px",
+          {
+            fontWeight: "700",
+            lineHeight: "38.73px",
+          },
+        ],
+        "app-nav-link": [
+          "16px",
+          {
+            fontWeight: "600",
+            lineHeight: "19.36px",
+          },
+        ],
+        "app-form-title": [
+          "32px",
+          {
+            fontWeight: "600",
+            lineHeight: "38.73px",
+          },
+        ],
+        "app-form-sub-text-1": [
+          "16px",
+          {
+            fontWeight: "400",
+            lineHeight: "19.36px",
+          },
+        ],
+        "app-form-sub-text-2": [
+          "16px",
+          {
+            fontWeight: "400",
+            lineHeight: "26px",
+          },
+        ],
+        "app-form-footer-label": [
+          "16px",
+          {
+            fontWeight: "400",
+            lineHeight: "19.36px",
+          },
+        ],
+        "app-form-footer-cta": [
+          "16px",
+          {
+            fontWeight: "500",
+            lineHeight: "19.36px",
+            letterSpacing: "0.07em",
+          },
+        ],
+        "app-toolbar-link": [
+          "12px",
+          {
+            fontWeight: "400",
+            lineHeight: "14.52px",
+          },
+        ],
+        "app-nav-banner": [
+          "14px",
+          {
+            fontWeight: "500",
+            lineHeight: "16.94px",
+          },
+        ],
+      },
+      colors: {
+        charcoal: "#333333",
+        silver: "#F4F4F4",
       },
     },
   },
